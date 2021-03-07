e6693dea132efe2fc65468fe885a8d492decc2c1
heathen29 <diversant329@gmail.com>
2021-03-08 00:34:28 +0200

    Lesson 4. Blend Tree + State Machine configuration.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _hp = 100.0f;
+
+    public float HP 
+    {
+        get 
+        {
+            return _hp;
+        }
+    }
+
+    public void PlayStep() {}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
