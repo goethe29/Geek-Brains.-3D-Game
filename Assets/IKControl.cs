02a4fc92c77359d797bf17fae3f7f92b4b9e6e13
heathen29 <diversant329@gmail.com>
2021-03-11 00:23:01 +0200

    Lesson 5. IK Script to move Right Hand + set look object for the head. Ragdoll script to kill character.

---
+++ Assets/IKControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private bool _useIK;
+    [SerializeField] Transform _rightHandGoal;
+    [SerializeField] float _rightHandWeight;
+    [SerializeField] Transform _lookGoal;
+    [SerializeField] float _lookDistance = 2;
+    
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex) {
+        if (_animator)
+        {
+            if (_useIK)
+            {
+                if (_rightHandGoal)
+                {
+                    _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, _rightHandWeight);
+                    //_animator.SetIKRotationWeight(AvatarIKGoal.RightHand, _rightHandWeight);
+                    _animator.SetIKPosition(AvatarIKGoal.RightHand, _rightHandGoal.position);
+                    //_animator.SetIKRotation(AvatarIKGoal.RightHand, _rightHandGoal.rotation);
+                }
+
+                if (_lookGoal)
+                {
+                    LookAt();
+                }
+            }
+            else
+            {
+                ResetIK();
+            }
+        }
+    }
+
+    private void LookAt()
+    {
+        float distance = Vector3.SqrMagnitude(gameObject.transform.position - _lookGoal.transform.position);
+        if (distance <= _lookDistance *_lookDistance)
+        {
+            _animator.SetLookAtWeight(1);
+            _animator.SetLookAtPosition(_lookGoal.position);
+        }
+        else
+        {
+            ResetIK("Head");
+        }    
+    }
+
+    private void ResetIK(string nameIK = "All") 
+    {
+        if (nameIK == "All" || nameIK == "Head")
+        {   
+            _animator.SetLookAtWeight(0);
+        }
+
+        if (nameIK == "All" || nameIK == "RightHand")
+        {   
+            _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+            _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+        }
+    }
+}
---
+++ Assets/RagdollControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollControl : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _parentBody;
+    [SerializeField] private Rigidbody[] _rbs;
+    [SerializeField] private Collider[] _colliders;
+    [SerializeField] private Animator _animator;
+
+    private void Awake() 
+    {
+        _animator = GetComponent<Animator>();
+        _rbs = GetComponentsInChildren<Rigidbody>();
+        _colliders = GetComponentsInChildren<Collider>();
+        Revive();
+        Invoke("Kill", 3.0f);
+    }
+
+    private void SetRagdoll(bool active) 
+    {
+        for (int i = 0; i < _rbs.Length; i++)
+        {
+            _rbs[i].isKinematic = !active;
+        }
+        for (int i = 0; i < _colliders.Length; i++)
+        {
+            _colliders[i].enabled = active;
+        }
+    }
+
+    private void SetMain(bool active) 
+    {
+        _animator.enabled = active;
+        _rbs[0].isKinematic = !active;
+        _colliders[0].enabled = active;
+    }
+
+    private void Kill() 
+    {
+        SetRagdoll(true);
+        SetMain(false);
+    }
+
+    private void Revive() 
+    {
+        SetRagdoll(false);
+        SetMain(true);
+    }
+}
